32f373d6185c491cff5dd9264c9e0382b82d2fe9
m7mdra <xm7mdrax@gmail.com>
2020-11-17 22:08:17 +0200

    added swatch colors

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,27 @@ import 'package:flutter_svg/flutter_svg.dart';
 void main() {
   runApp(MyApp());
 }
+ const MaterialColor _swatch = MaterialColor(_swatchPrimaryValue, <int, Color>{
+  50: Color(0xFFF3F7E3),
+  100: Color(0xFFE1EBB9),
+  200: Color(0xFFCEDE8B),
+  300: Color(0xFFBAD15D),
+  400: Color(0xFFABC73A),
+  500: Color(_swatchPrimaryValue),
+  600: Color(0xFF94B714),
+  700: Color(0xFF8AAE11),
+  800: Color(0xFF80A60D),
+  900: Color(0xFF6E9807),
+});
+ const int _swatchPrimaryValue = 0xFF9CBD17;
 
+ const MaterialColor swatchAccent = MaterialColor(_swatchAccentValue, <int, Color>{
+  100: Color(0xFFECFFC5),
+  200: Color(_swatchAccentValue),
+  400: Color(0xFFCBFF5F),
+  700: Color(0xFFC3FF46),
+});
+ const int _swatchAccentValue = 0xFFDCFF92;
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,8 +34,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(),
       theme: ThemeData(
         fontFamily: 'cairo',
-        primaryColorDark: Color(0xff9cbd17),
-        primaryColor: Color(0xff9cbd17),
+        primarySwatch: _swatch,
+        primaryColor: Color(_swatchPrimaryValue),
         backgroundColor: Color(0xFFFCFCFC),
         appBarTheme: AppBarTheme(centerTitle: true, elevation: 0),
         visualDensity: VisualDensity.adaptivePlatformDensity,
