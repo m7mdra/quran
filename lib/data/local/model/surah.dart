1cdd17fab2fc4e1447c09c96682a918a9568c018
m7mdra <xm7mdrax@gmail.com>
2021-02-05 23:56:50 +0200

    navigation from surat list -> quran reader page

--- lib/data/local/model/surah.dart
+++ lib/data/local/model/surah.dart
@@ -1,18 +1,18 @@
 class Surah {
-  Surah(
-      {this.number,
-      this.name,
-      this.englishName,
-      this.englishNameTranslation,
-      this.revelationType,
-      this.numberOfAyat});
+  Surah({this.number,
+    this.name,
+    this.englishName,
+    this.englishNameTranslation,
+    this.revelationType,
+    this.page,
+    this.numberOfAyat});
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is Surah &&
-          runtimeType == other.runtimeType &&
-          number == other.number;
+          other is Surah &&
+              runtimeType == other.runtimeType &&
+              number == other.number;
 
   @override
   int get hashCode => number.hashCode;
@@ -23,18 +23,23 @@ class Surah {
   String englishNameTranslation;
   String revelationType;
   int numberOfAyat;
+  int page;
 
-  factory Surah.fromMap(Map<String, dynamic> map) => Surah(
-      number: map["id"],
-      name: map["name"],
-      englishName: map["englishName"],
-      englishNameTranslation: map["englishtranslation"],
-      revelationType: map["revelationType"],
-      numberOfAyat: map['numberOfAyats']);
+  factory Surah.fromMap(Map<String, dynamic> map) =>
+      Surah(
+          number: map["id"],
+          name: map["name"],
+          page: map['page_id'],
+          englishName: map["englishName"],
+          englishNameTranslation: map["englishtranslation"],
+          revelationType: map["revelationType"],
+          numberOfAyat: map['numberOfAyats']);
 
-  Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() =>
+      {
         "number": 'id',
         "name": name,
+        "page_id": page,
         "englishName": englishName,
         "englishNameTranslation": englishNameTranslation,
         "revelationType": revelationType,
